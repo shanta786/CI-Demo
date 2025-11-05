75c868e0b8a478b0ddc789cc586851b5b9348a5e
Shanta Sabuj Sadhukhan <84620854+shanta786@users.noreply.github.com>
2025-11-05 22:21:18 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(2,3) == 5
 
 def test_divide():
-    assert divide(6,4) == 2
+    assert divide(6,3) == 2
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
